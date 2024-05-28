b3428c30df7ca18519cb72e2c07ecd00505f7ea7
CarlosRueda7 <110608022+CarlosRueda7@users.noreply.github.com>
2024-05-27 23:09:46 -0500

    interfaz
    nueva

--- fotos.py
+++ fotos.py
@@ -1,51 +1,51 @@
-# streamlit_audio_recorder y whisper by Alfredo Diaz - version Mayo 2024
+# streamlit_audio_recorder y whisper por Alfredo Diaz - versión Mayo 2024
 
-# En VsC seleccione la version de Python (recomiendo 3.9) 
-#CTRL SHIFT P  para crear el enviroment (Escriba Python Create Enviroment) y luego venv 
+# En VsC seleccione la versión de Python (recomiendo 3.9)
+# CTRL SHIFT P para crear el entorno (Escriba Python Create Enviroment) y luego venv
 
-#o puede usar el siguiente comando en el shell
-#Vaya a "view" en el menú y luego a terminal y lance un terminal.
-#python -m venv env
+# o puede usar el siguiente comando en el shell
+# Vaya a "view" en el menú y luego a terminal y lance un terminal.
+# python -m venv env
 
-#Verifique que el terminal inicio con el enviroment o en la carpeta del proyecto active el env.
-#cd D:\flores\env\Scripts\
-#.\activate 
+# Verifique que el terminal inició con el entorno o en la carpeta del proyecto active el entorno.
+# cd D:\flores\env\Scripts\
+# .\activate 
 
-#Debe quedar asi: (.venv) D:\proyectos_ia\Flores>
+# Debe quedar así: (.venv) D:\proyectos_ia\Flores>
 
-#Puedes verificar que no tenga ningun libreria preinstalada con
-#pip freeze
-#Actualicie pip con pip install --upgrade pip
+# Puede verificar que no tenga ninguna librería preinstalada con
+# pip freeze
+# Actualice pip con pip install --upgrade pip
 
-#pip install tensorflow==2.15 La que tiene instalada Google Colab o con la versión qu fué entrenado el modelo
-#Verifique se se instaló numpy, no trate de instalar numpy con pip install numpy, que puede instalar una version diferente
-#pip install streamlit
-#Verifique se se instaló no trante de instalar con pip install pillow
-#Esta instalacion se hace si la requiere pip install opencv-python
+# pip install tensorflow==2.15 La que tiene instalada Google Colab o con la versión que fue entrenado el modelo
+# Verifique si se instaló numpy, no trate de instalar numpy con pip install numpy, que puede instalar una versión diferente
+# pip install streamlit
+# Verifique si se instaló, no trate de instalar con pip install pillow
+# Esta instalación se hace si la requiere pip install opencv-python
 
-#Descargue una foto de una flor que le sirva de ícono 
+# Descargue una foto de un estudiante que le sirva de ícono
 
-# importing the libraries and dependencies needed for creating the UI and supporting the deep learning models used in the project
+# Importación de las librerías y dependencias necesarias para crear la interfaz de usuario y soportar los modelos de aprendizaje profundo usados en el proyecto
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import streamlit as st  
-import tensorflow as tf # TensorFlow is required for Keras to work
+import tensorflow as tf # TensorFlow es necesario para que Keras funcione
 from PIL import Image
 import numpy as np
-#import cv2
+# import cv2
 
-# hide deprication warnings which directly don't affect the working of the application
+# Ocultar advertencias de deprecación que no afectan directamente el funcionamiento de la aplicación
 import warnings
 warnings.filterwarnings("ignore")
 
-# set some pre-defined configurations for the page, such as the page title, logo-icon, page loading state (whether the page is loaded automatically or you need to perform some action for loading)
+# Configurar algunas configuraciones predefinidas para la página, como el título de la página, el ícono del logo y el estado de carga de la página (si la página se carga automáticamente o si necesita realizar alguna acción para cargarla)
 st.set_page_config(
-    page_title="Reconocimiento de Flores",
-    page_icon = ":smile:",
-    initial_sidebar_state = 'auto'
+    page_title="Reconocimiento Facial de Estudiantes",
+    page_icon=":smile:",
+    initial_sidebar_state='auto'
 )
 
-# hide the part of the code, as this is just for adding some custom CSS styling but not a part of the main idea 
+# Ocultar parte del código, ya que esto es solo para agregar algo de estilo CSS personalizado, pero no es parte de la idea principal
 hide_streamlit_style = """
 	<style>
   #MainMenu {visibility: hidden;}
@@ -53,53 +53,48 @@ hide_streamlit_style = """
   </style>
 """
 
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) # Oculta el código CSS de la pantalla, ya que están incrustados en el texto de rebajas. Además, permita que Streamlit se procese de forma insegura como HTML
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) # Oculta el código CSS de la pantalla, ya que están incrustados en el texto markdown. Además, permite que Streamlit se procese de forma insegura como HTML
 
-#st.set_option('deprecation.showfileUploaderEncoding', False)
+# st.set_option('deprecation.showfileUploaderEncoding', False)
 @st.cache_resource
 def load_model():
-    model=tf.keras.models.load_model('./fotos_procesadas.h5')
+    model = tf.keras.models.load_model('./fotos_procesadas.h5')
     return model
-with st.spinner('Modelo está cargando..'):
-    model=load_model()
-    
 
+with st.spinner('El modelo se está cargando..'):
+    model = load_model()
 
 with st.sidebar:
-        st.image('rosa.jpg')
-        st.title("Reconocimiento de imagen")
-        st.subheader("Reconocimiento de imagen para flores")
-        st.markdown(
-            """
-            <span style='color:red'>This is First page
-            You can:
-            - Say Hi
-            - Send email
-            - Contact crueda578@unab.edu.co </span>
-            """,
-            unsafe_allow_html=True
-            )
-        confianza=st.slider("Seleccione el nivel de confianza %", 0,100,50)
+    st.image('rosa.jpg')
+    st.title("Reconocimiento Facial")
+    st.subheader("Reconocimiento de rostros de estudiantes de la clase de Ciencias de Datos")
+    st.markdown(
+        """
+        <span style='color:red'>Esta es la primera página
+        Usted puede:
+        - Decir Hola
+        - Enviar un correo
+        - Contactar a crueda578@unab.edu.co </span>
+        """,
+        unsafe_allow_html=True
+    )
+    confianza = st.slider("Seleccione el nivel de confianza %", 0, 100, 50)
 
-col1,col2,col3=st.columns(3)
+col1, col2, col3 = st.columns(3)
 with col2:
     st.image('logo.jpg')
-    
-st.title("Unab")
-st.write("CarlosRueda")
+
+st.title("Universidad Autónoma de Bucaramanga")
+st.write("Anghel Gutiérrez y Carlos Rueda")
 st.write("""
-         # Detección de flores
-         """
-         )
+         # Reconocimiento Facial de Estudiantes
+         """)
 
 def import_and_predict(image_data, model, class_names):
-    
     image_data = image_data.resize((180, 180))
-    
     image = tf.keras.utils.img_to_array(image_data)
-    image = tf.expand_dims(image, 0) # Create a batch
+    image = tf.expand_dims(image, 0) # Crear un lote
 
-    
     # Predecir con el modelo
     prediction = model.predict(image)
     index = np.argmax(prediction)
@@ -108,10 +103,9 @@ def import_and_predict(image_data, model, class_names):
     
     return class_name, score
 
-
 class_names = open("./clases.txt", "r").readlines()
 
-img_file_buffer = st.camera_input("Capture una foto para identificar una flor")    
+img_file_buffer = st.camera_input("Capture una foto para identificar a un estudiante")
 if img_file_buffer is None:
     st.text("Por favor tome una foto")
 else:
@@ -122,10 +116,8 @@ else:
     class_name, score = import_and_predict(image, model, class_names)
     
     # Mostrar el resultado
-
-    if np.max(score)>0.5:
-        st.subheader(f"Tipo de Flor: {class_name}")
+    if np.max(score) > 0.5:
+        st.subheader(f"Estudiante Identificado: {class_name}")
         st.text(f"Puntuación de confianza: {100 * np.max(score):.2f}%")
     else:
-        st.text(f"No se pudo determinar el tipo de flor")
-
+        st.text("No se pudo identificar al estudiante")
